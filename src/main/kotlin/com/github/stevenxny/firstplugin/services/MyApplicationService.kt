66828608678d0aabcfaba42ed82d618e56fe777d
GitHub Action <action@github.com>
2021-05-18 14:15:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/stevenxny/firstplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/stevenxny/firstplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.stevenxny.firstplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stevenxny.firstplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/stevenxny/firstplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stevenxny/firstplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stevenxny.firstplugin.services
 
+import com.github.stevenxny.firstplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
